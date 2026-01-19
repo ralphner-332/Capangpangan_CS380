78425af33c22c480c5a9dcca837a3eed5ace09be
Ralphner <ralphner.capangpangan@urios.edu.ph>
2026-01-19 03:20:39 +0000

    all file changed

---
+++ lox/Scanner.java
@@ -0,0 +1,40 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static com.craftinginterpreters.lox.TokenType.*; // belongs to the class
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+    this.source = source;
+    }
+    
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            scanToken();
+            }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+        }
+    
+    private boolean isAtEnd() {
+        return current >= source.length();
+    
+    private void scanToken() {
+        char c = advance();
+        }
+
+
+
+    }
+}
